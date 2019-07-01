950fe151f1ecf629fbb46dcd1017d631aa403786
Callum Parker <callum.parker@gmail.com>
2019-07-01 16:55:47 +1000

    Update gsscraper.py
    Update bringing this code into 2019.
    - It now properly downloads pdfs and doesn't crash when getting a 404.
    - Increased the delay.
    - Updated the Scholar link.

--- gsscraper.py
+++ gsscraper.py
@@ -3,28 +3,27 @@ from random import randint
 from time import sleep
 import requests
 import urllib
+import wget
 
-testfile = urllib.URLopener()
+paperFile = urllib.URLopener()
 
 numPages = input("How many pages do you want to scrape? ")
+searchQuery = raw_input("Search query (separated by +)? ")
 file = open("pdflinks.txt", "w")
 paperCount = 0
 
 for x in range(0, int(numPages)):
-	sleep(randint(1,2))
+	sleep(randint(1,5))
 	print("PAGE NUM: " + str(x))
-	page = requests.get('https://scholar.google.com.au/scholar?start=' + str(x * 10) + '&q=source:Pervasive+source:Displays&hl=en&as_sdt=0,5&as_ylo=2016')
+	page = requests.get('https://scholar.google.com.au/scholar?start=' + str(x * 10) + '&q=' + searchQuery)
 	tree = html.fromstring(page.content)
 	hrefs = tree.xpath('//div[@class="gs_or_ggsm"]/a')
 
 	for href in hrefs:
 		print(href.attrib['href'])
 		file.write(href.attrib['href'] + "\n")
-		testfile.retrieve(href.attrib['href'], "paper" + str(paperCount) + ".pdf")
+		paperFile = wget.download(href.attrib['href'], "paper" + str(paperCount) + ".pdf")
 		paperCount = paperCount + 1
 
 file.close()
-print("Successfully gathered the links!")	
-
-# https://scholar.google.com.au/scholar?start=' + str(x * 10) + 'as_vis=1&q=%22public+display%22+%22digital+signage%22+personalisation&hl=en&as_sdt=1,5
-# https://scholar.google.com.au/scholar?start=' + str(x * 10) + '&q=%22public+display%22+%22digital+signage%22+observation+-education+-cost&hl=en&as_sdt=1,5&as_vis=1
+print("Successfully gathered the links!")
